273b75c5b0e13080404ccf9f7698749e2da27996
veronica <v.naranjo@utp.edu.co>
2025-03-24 17:22:26 -0500

    Añadir script principal `run.py` para iniciar la aplicación PyQt6

---
+++ SRC/run.py
@@ -0,0 +1,12 @@
+from ui.views.visor import VisorImagenes
+from PyQt6.QtWidgets import QApplication
+import sys
+
+def main():
+    app = QApplication(sys.argv)
+    ventana = VisorImagenes()
+    ventana.show()
+    sys.exit(app.exec())
+        
+if __name__ == "__main__":
+    main()
